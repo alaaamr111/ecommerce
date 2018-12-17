4c6e0a7a3d46d614091bfa3fe81212a02fd289da
Alaa amr <41928157+Alaaamr1@users.noreply.github.com>
2018-12-17 04:13:28 +0200

    jj
    j

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+ $("#search").click( function(){
+     $("#nav3").toggle( 3000 , function(){
+      });
+    });
+     
+     $("#categorydorpdown").hover(function(){
+      $("#main").toggle();
+      });
+     
+     $("#books").hover(function(){
+      $("#bks").toggle();
+      });
+      $("#electronics").hover(function(){
+      $("#bks2").toggle();
+      });
+       $("#health").hover(function(){
+      $("#bks3").toggle();
+      });
+    
+   
